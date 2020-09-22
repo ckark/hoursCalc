7873035fb03bc8a3dafe30618c8249a18067a4b6
Chris Kark <ckark@alumni.stanford.edu>
2020-09-22 17:08:47 -0600

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # hoursCalc
- Calculate Your Availability Each Quarter
+
+hoursCalc is a quick way of estimating your availability each quarter based on holidays and any vacation days you take. 
+
+The formula powering the algorithm works as follows:
+```
+/* fixed variables */
+const weeks = 12; // weeks in an average quarter
+const hours = 40; // average weekly work hours
+const workDay = 8; // average hours in a workday
+const nonDeepWork = 0.20; // 20% of time in meetings and on "non-deep" work
+
+/* flexible variables */
+let holidays = 0; // paid holidays this quarter
+let vacationDays = 0; // vacation days this quarter
+
+/* formula */
+const availability = () => ((weeks * hours) - ((weeks * hours) * nonDeepWork)) - ((vacationDays + holidays) * workDay);
+
+/* calculate total */
+availability(); // n available hours this quarter
+```
+
+Try it out on [JSBin](https://jsbin.com/zamama/edit?js,console).
--- main.js
+++ main.js
@@ -1,40 +1,36 @@
 Array.from(document.getElementsByClassName("numbers")).forEach((e) => {
   e.addEventListener("input", function () {
     this.value = this.value.replace(/[^\d]/g, "");
-  });
-  e.addEventListener("keydown", (e) => {
-    e = window.event || e;
-    let key = e.keyCode;
-    key == 32 ? e.preventDefault() : true;
-  });
+  }),
+    e.addEventListener("keydown", (e) => {
+      32 != (e = window.event || e).keyCode || e.preventDefault();
+    });
 });
-
-const total = (h, v, a) => {
-  (h = document.getElementById(h)),
-    (v = document.getElementById(v)),
-    ((a = document.getElementById(a)).textContent = "0"),
-    h.addEventListener("input", () => {
-      (a.textContent = 384 - 8 * (Number(h.value) + Number(v.value))),
-        "NaN" === a.textContent && (a.textContent = "0");
+const total = (e, t, n) => {
+  (e = document.getElementById(e)),
+    (t = document.getElementById(t)),
+    ((n = document.getElementById(n)).textContent = "0"),
+    e.addEventListener("input", () => {
+      (n.textContent = 384 - 8 * (Number(e.value) + Number(t.value))),
+        "NaN" === n.textContent && (n.textContent = "0");
     }),
-    v.addEventListener("input", () => {
-      (a.textContent = 384 - 8 * (Number(h.value) + Number(v.value))),
-        "NaN" === a.textContent && (a.textContent = "0");
+    t.addEventListener("input", () => {
+      (n.textContent = 384 - 8 * (Number(e.value) + Number(t.value))),
+        "NaN" === n.textContent && (n.textContent = "0");
     });
 };
 total("holidays", "vacationDays", "availability");
-const validate = (t) => {
-  Array.from(document.getElementsByClassName(t)).forEach((t) => {
-    t.addEventListener("blur", function () {
-      !1 === t.checkValidity() || "" === t.value
-        ? (t.classList.add("error"),
+const validate = (e) => {
+  Array.from(document.getElementsByClassName(e)).forEach((e) => {
+    e.addEventListener("blur", function () {
+      !1 === e.checkValidity() || "" === e.value
+        ? (e.classList.add("error"),
           setTimeout(function () {
-            t.classList.remove("error");
+            e.classList.remove("error");
           }, 300),
           event.preventDefault())
-        : (t.style.backgroundColor = null);
+        : (e.style.backgroundColor = null);
     });
   });
 };
-
 validate("numbers");
